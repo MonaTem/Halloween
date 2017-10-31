845ab23f2ad340be88ba3a4c7b51641c6244db8f
Mona Temchin <monatemchin@gmail.com>
2017-10-31 09:58:42 -0400

    history added

---
+++ history.js
@@ -0,0 +1,19 @@
+var commandArray =
+[
+'git status',
+ 'git add halloween.js',
+ 'git status',
+ 'git commit -m /"costumes',
+ 'git remote -v',
+ 'git push -u https://github.com/MonaTem/Halloween.git',
+ 'ls -a',
+ 'echo "# Halloween" >> README.md',
+ 'git add README.md',
+ 'git status',
+ 'git commit -m "added README"',
+ 'git push',
+ 'cat README.md',
+ 'ls -a',
+  'cat halloween.js',
+ 'history'
+];
