72a62184ef1eaf5208eb763e021e21a4a832d692
Mona Temchin <monatemchin@gmail.com>
2017-10-31 09:39:36 -0400

    costumes

--- halloween.js
+++ halloween.js
@@ -1,3 +1,5 @@
 var candyArray =
  ["chocolate truffles", "Reece's peanut butter cups",
  "Reece's pieces", "Bacchi"];
+var costumeArray = ["unicorn", "The Grim Reaper", "cat",
+                    "ladybug"];
